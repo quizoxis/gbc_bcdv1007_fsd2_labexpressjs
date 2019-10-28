7aa544c84c9ab52ba647602268a28a5acf983a91
Quiz Oxis <quiz.oxis@gmail.com>
2019-10-27 22:51:49 -0400

    Added 1_express

---
+++ 1_express/server/appFilename.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const appServer = express();
+const port = 999
+
+appServer.get('/', (req, res) => res.send('Hello World'));
+appServer.listen(port, () => console.log(`Ok on port ${port}`));
